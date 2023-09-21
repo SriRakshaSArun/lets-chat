0e79e7beeb925886e6db4dc90f582ae4502d6f2e
EndUser <sriraksha.arun@gmail.com>
2023-09-21 14:48:14 -0700

    add comments to the server file

--- server.js
+++ server.js
@@ -15,19 +15,23 @@ app.get('/', (req, res) => {
     res.send("Server is up...");
 })
 
+// Create the server events
 io.on('connection',(socket)=>{
     console.log('New connection established...');
 
+    // Join room event
     socket.on('joinRoom', function(data){
       socket.join(data.room);
       console.log(data.user + 'joined the room : ' + data.room);
       socket.broadcast.to(data.room).emit('newUserJoined', {user:data.user, message:'has joined this room.'});
     });
     
+    // Message event
     socket.on('message',function(data){
       io.in(data.room).emit('newMessage', {user:data.user, message:data.message});
     });
 
+    // Leave event
     socket.on('leave', function(data){
       console.log(data.user + 'left the room : ' + data.room);
       socket.broadcast.to(data.room).emit('leaveRoom', {user:data.user, message:'has left this room.'});
